17cd72c0a25fbb43389f8362587650d89e585b67
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2021-08-29 17:07:30 -0600

    [NUEVO] Comentarios 🌚
    Se agregan los comentarios a las funciones

--- Optimización/Octave/gradiente_aux.m
+++ Optimización/Octave/gradiente_aux.m
@@ -4,24 +4,37 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+% Funcion auxiliar para llamar a la funcion de gradiente
+%   En esta función se definen los atributos que la funcion
+%   gradiente necesita para calcular la minimizacion
 function gradiente_aux
   clc; clear; %se limpia la consola
   format long; %se configura el formato long para el resultado numÃ©rico
-  warning off;
+  warning off; %se desactivan los mensajes de advertencia
   
   f = '(x - 2)^4 + (x - 2*y)^2'; % se define la funciÃ³n a utilizar.
-  x0 = [0, 3];
-  variables = ['x', 'y'];
-  tol = 10^-3;
-  iterMax = 13;
+  x0 = [0, 3]; %se define el punto inicial
+  variables = ['x', 'y']; %se definen las variables a utilizar
+  tol = 10^-3; %se define la tolerancia
+  iterMax = 13; %se define el maximo de iteraciones
   
+  %se llama a la funcion
   [xk error] = gradiente(f, x0, variables, iterMax, tol)
-  
-  
   
 end
 
 
+% Metodo numérico gradiente conjugado no lineal
+% para encontrar al menos un punto minimo en una
+% función f multivariable a partir de un valor inicial dado
+% Además esta función grafica las iteraciones vs el error.
+% f: la funcion a aplicarle el método
+% x0: el punto inicial para comenzar el método
+% variables: las variables con las que se trabaja la funcion f
+% tol: valor de la tolerancia de resultado aceptable
+% iterMax: cantidad máxima de iteraciones que se pueden realizar
+%
+% Retorno: un vector con la aproximación y el error.
 function [xk error] = gradiente(f, x0, variables, iterMax, tol)
   %cargar el paquete symbolic
   pkg load symbolic
@@ -31,25 +44,40 @@ function [xk error] = gradiente(f, x0, variables, iterMax, tol)
   er = []; %se crea un vector para los errores
   iter = []; %se crea un vector con las iteraciones
   error = tol + 1; %se define un valor inicial para el error
-  xk = x0';
   
-  
-  numVariables = length(variables);
+  xk = x0'; %se define el punto para la iteracion inicial
+            % importante recalcar que es la transpuesta
+            % del punto x0
+            
+  numVariables = length(variables); %se define el numero de
+                                    %variables a trabajar
   symbolicas = []; %lista de variables simbolicas
   
-  grad = gradient(f);
+  grad = gradient(f); %se calcula el gradiente
   
+  %se convierte a cada una de las variables
+  %ingresadas a variables simbolicas
   for n=1: numVariables
     symbolicas = [symbolicas, sym(variables(n))];
   endfor
   
+   %se evalua el gradiente en el punto inicial
+   % el resultado es un vector columna
   gk = [double(subs(grad, symbolicas, xk'))];
+  
+  %se define el vector dk
+  % este es un vector fila
   dk = -gk;
   
   iterMax2 = 1000; %iteraciones maximas para el valor de alpha_k
   
+  %se calcula el error inicial y se agrega al
+  %vector de errores
   error = norm(double(subs(grad, symbolicas, xk')));
   er = [er, error];
+  
+  %se agrega la primera iteracion al vector
+  % de iteraciones
   iter = [iter, 0];
   for k = 0: iterMax
     k = k + 1;
@@ -57,32 +85,57 @@ function [xk error] = gradiente(f, x0, variables, iterMax, tol)
     delta = 0.5; %se define delta
     alpha_k = 1; %se define alpha_k
     
+    %primero se debe calcular el valor de alpha_k
+    %para poder calcular el valor de la siguiente
+    %iteracion
+    %esto se hace mediante la siguiente validacion
     for i = 1: iterMax2
+      
+      %se define un vector para 
+      %evaluar la funcion ingresada
       vectorEval = xk + alpha_k*dk;
-
-      izq = double(subs(f, symbolicas, vectorEval') - subs(f, symbolicas, xk'));
+      
+      %se tiene una ecuacion en la que se tiene que evaluar 
+      %que el lado izquierdo sea menor o igual que el lado derecho
+      
+      %se calcula el lado izquierdo
+      izq = double(subs(f, symbolicas, vectorEval') 
+            - subs(f, symbolicas, xk'));
+            
+      %se calcula el lado derecho
       der = delta*alpha_k*(gk)'*dk;
       if izq <= der
         break
       else
-        alpha_k = alpha_k/2;
+        alpha_k = alpha_k/2; %se define un valor mas
+                             %pequeno para alpha_k
       endif
     endfor
     
-    xk = xk + (alpha_k*dk);
+    xk = xk + (alpha_k*dk); %se calcula la nueva aproximacion
+    
+    %se calcula el error y se agrega al
+    % vector de errores
     error = norm([double(subs(grad, symbolicas, xk'))]);
     er = [er, error];
     if error < tol
       break
     endif
+    
+    % se calcula el valor de beta
+    % esto se hace por medio del grediente
+    % evaluado en la iteracion anterior
+    % y el gradiente evaluado en el punto nuevo 
     gk_anterior = gk;
     gk = double(subs(grad, symbolicas, xk'));
     
     beta_k = (norm(gk)^2)/(norm(gk_anterior)^2);
+    
+    %se calcula el nuevo valor de dk
     dk = -gk + beta_k*dk;   
   endfor
   
-  
+  %se grafica las iteraciones vs el error
   plot(iter, er)
   
 end
--- Optimización/Python/coordinado.py
+++ Optimización/Python/coordinado.py
@@ -1,8 +1,15 @@
 import numpy as np
 import sympy as sp
 from scipy import optimize
-import json
+import matplotlib.pyplot as plt
 
+"""
+Metodo para convertir una serie de variables
+ingresadas a variables simbolicas
+variables: las varianbles en un string
+            en formato 'x y z'
+Retorna: una lista con las variables simbolicas
+"""
 def variables_simbolicas(variables):
     n = len(variables)
     tam = np.arange(0, n, 2)
@@ -11,7 +18,14 @@ def variables_simbolicas(variables):
         v_var.append(sp.Symbol(variables[i]))
     return v_var
 
-
+"""
+Metodo para calcular el gradiente de una funcion dada
+y retornarlo en su forma simbolica
+f: la funcion a calcularle su gradiente
+v_var: variables simbolicas para calcular el gradiente
+Retorna: el gradiente en forma simbolica
+         dentro de una lista
+"""
 def gradiente(f, v_var):
     n = len(v_var)
     g = []
@@ -20,28 +34,48 @@ def gradiente(f, v_var):
     return g
 
 
+"""
+Metodo numérico descenso coordinado para encontrar 
+al menos un minimo de una función f multivariable
+a partir de un valor inical (x0, y0, ...).
+Además esta función grafica las iteraciones vs el error.
+funcion: la funcion a aplicarle el método de optimizacion
+variables: Representan todas las variables simbolicas
+           con las que se va a trabajar.
+puntoInicial: punto de referencia inicial para comenzar
+              a aplicar el metodo
+tol: valor de la tolerancia de resultado aceptable
+iterMax: cantidad máxima de iteraciones que se pueden realizar
+Retorno: un vector con la aproximación y el error.
+"""
 def coordinado(funcion, variables, puntoInicial, iterMax, tol):
     #funciones
-    f_s = sp.sympify(funcion)
+    f_s = sp.sympify(funcion) #simbolica
 
-    #gradirente
+    #gradirente (simbolico)
     g = gradiente(f_s, variables)
-    g_n = sp.lambdify(variables, g)
 
     #punto inicial
     xk = puntoInicial
 
+    #se accede al numero de variables
     numVariables = len(variables)
 
+    #vector para guardar el error
+    er = []
+
     k = 0
-    
     while k < iterMax:
         k += 1
         n = 0
         indexSuperior = numVariables - 1
+        
+        #por cada variable se "fijan" las demas
+        # y se calcula el nuevo punto xk
         while n < numVariables:
-            
-            simbolica = f_s.subs(variables[indexSuperior], xk[indexSuperior])
+            #se usa el metodo de Gauss-Seidel
+            simbolica = f_s.subs(variables[indexSuperior], 
+                        xk[indexSuperior])
 
             funcion_num = sp.lambdify(variables[n], simbolica)
 
@@ -53,6 +87,8 @@ def coordinado(funcion, variables, puntoInicial, iterMax, tol):
 
             indexSuperior -= 1
         
+        #se utiliza para evaluar el gradiente
+        #de acuerdo al numero de variables existente
         data ={}
         g_evaluado = []
         for i in range(0, numVariables):
@@ -61,11 +97,17 @@ def coordinado(funcion, variables, puntoInicial, iterMax, tol):
         for i in range(0, numVariables):
             g_evaluado.append(float(g[i].subs(data).evalf()))
 
+        #se calcula el error y se agrega
+        #al vector de errores
         error = np.linalg.norm(g_evaluado)
+        er.append(error)
         if error < tol:
             break
 
-    print(xk, error)
+    fig, graf = plt.subplots() #se crea la gráfica
+    ejeX = np.arange(1, k+1, 1) #se crea el eje X (son las iteraciones)
+    graf.plot(ejeX, er) #se grafican los datos
+    plt.show() #se muestra la gráfica
     return [xk, error]
 
 
