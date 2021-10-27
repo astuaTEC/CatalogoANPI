c99cf00eb375e5ba4b7807ba35b997da061092d9
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2021-10-27 16:28:54 -0600

    [NUEVO] Métodos

---
+++ Diferenciación numérica/Octave/euler.m
@@ -0,0 +1,23 @@
+%% Ejemplo del Mè´±todo de Euler
+clc; clear; close all
+a=0; b=5;
+num_pt=11;
+f=@(x,y) y-x.^2+1;
+h=(b-a)/(num_pt-1);
+xv=a:h:b;
+yv=[0.5];
+for n=1:num_pt-1  
+  yv(n+1)=yv(n)+h*f(xv(n),yv(n));
+end
+
+
+%Grè´°fica de la aproximaciè´—n
+hold on
+plot(xv,yv,'r')
+
+%Soluciè´—n analè´øtica
+y_s=@(x) (x+1).^2-0.5*exp(x);
+%Graficar la solucion
+x_g=a:0.0001:b;
+y_g=y_s(x_g);
+plot(x_g,y_g,'b')
---
+++ Diferenciación numérica/Octave/predictor_corrector.m
@@ -0,0 +1,26 @@
+%Predictor corrector
+
+clc; clear; close all
+a=0; b=5;
+num_pt=11;
+f=@(x,y) y-x.^2+1;
+h=(b-a)/(num_pt-1);
+xv=a:h:b;
+yv=[0.5];
+zv = [0];
+for n=1:num_pt-1 
+  zv(n+1) = yv(n)+h*f(xv(n),yv(n));
+  yv(n+1)= yv(n) + h/2*(f(xv(n),yv(n)) + f(xv(n+1),zv(n+1)));
+end
+
+
+%Grè´°fica de la aproximaciè´—n
+hold on
+plot(xv,zv,'r')
+
+%Soluciè´—n analè´øtica
+y_s=@(x) (x+1).^2-0.5*exp(x);
+%Graficar la solucion
+x_g=a:0.0001:b;
+y_g=y_s(x_g);
+plot(x_g,y_g,'b')
