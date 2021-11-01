3bd33d4ec6f87632fde3d92ff5aca2b686dcd79a
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2021-10-31 21:08:51 -0600

    [NUEVO] Se crea un formato para las funciones

--- Diferenciación numérica/Octave/euler.m
+++ Diferenciación numérica/Octave/euler.m
@@ -1,24 +1,46 @@
+function euler_aux
+  pkg load symbolic;
+  %% Ejemplo del M���todo de Euler
+  clc; clear; close all
+  intervalo = [0 5];
+  num_pt = 11;
+  funcion = 'y - x^2 + 1';
+  
+  [xv, yv] = euler(funcion, intervalo, num_pt)
+  
+   
+endfunction
 
-%% Ejemplo del M���todo de Euler
-clc; clear; close all
-a=0; b=5;
-num_pt=11;
-f=@(x,y) y-x.^2+1;
-h=(b-a)/(num_pt-1);
-xv=a:h:b;
-yv=[0.5];
-for n=1:num_pt-1  
-  yv(n+1)=yv(n)+h*f(xv(n),yv(n));
-end
+
+function [xv, yv] = euler(funcion, intervalo, n)
+  % Simbolico
+  syms x;
+  f_simbolica = sym(funcion);
+  
+  f = matlabFunction(f_simbolica);
+  
+  a = intervalo(1);
+  b = intervalo(2);
+  
+  h=(b-a)/(n-1);
+  xv=a:h:b;
+  yv=[0.5];
+  for n=1:n-1  
+    yv(n+1)=yv(n)+h*f(xv(n),yv(n));
+  end
+  
+  hold on
+  plot(xv, yv, 'r');
+  
+  %Soluci���n anal���tica
+  y_s=@(x) (x+1).^2-0.5*exp(x);
+  %Graficar la solucion
+  
+  x_g=a:0.0001:b;
+  y_g=y_s(x_g);
+  plot(x_g,y_g,'b')
+  
+endfunction 
 
 
-%Gr���fica de la aproximaci���n
-hold on
-plot(xv,yv,'r')
 
-%Soluci���n anal���tica
-y_s=@(x) (x+1).^2-0.5*exp(x);
-%Graficar la solucion
-x_g=a:0.0001:b;
-y_g=y_s(x_g);
-plot(x_g,y_g,'b')
