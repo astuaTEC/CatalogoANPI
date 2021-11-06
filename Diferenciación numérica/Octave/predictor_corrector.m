7cd8c2fd514d3a9f8c80fdb192dc11d1c4cb8aaa
JUARSO <juan09fut.js@gmail.com>
2021-11-05 23:52:31 -0600

    Feliz navidad

--- Diferenciación numérica/Octave/predictor_corrector.m
+++ Diferenciación numérica/Octave/predictor_corrector.m
@@ -1,25 +1,94 @@
 %Predictor corrector
+function predictor_corrector_aux
+  pkg load symbolic;
 
-clc; clear; close all
-a=0; b=5;
-num_pt=11;
-f=@(x,y) y-x.^2+1;
-h=(b-a)/(num_pt-1);
-xv=a:h:b;
-yv=[0.5];
-for i=1:num_pt-1 
-  zv = yv(i)+h*f(xv(i),yv(i));
-  yv(i+1)= yv(i) + h/2*(f(xv(i),yv(i)) + f(xv(i+1),zv));
+  clc; clear; close all
+  warning off;
+  
+  intervalo = [0 2];
+  num_pt = 11;
+  funcion = 'y - x^2 + 1';
+  yinicial = 0.5;
+  
+  [xv, yv, polinomioInterpolacion] = predictor_corrector(funcion, intervalo, num_pt, yinicial)
+  
+   
+endfunction
+
+
+function [xv, yv, polinomioInterpolacion] = predictor_corrector(funcion, intervalo, pasosh, yinicial)
+  % Simbolico
+  syms x;
+  f_simbolica = sym(funcion);
+  
+  n = pasosh;
+  
+  f = matlabFunction(f_simbolica);
+  
+  a = intervalo(1);
+  b = intervalo(2);
+  
+  h=(b-a)/(n-1);
+  xv=a:h:b;
+  yv=[yinicial];
+  for i=1:n-1 
+    zv = yv(i)+h*f(xv(i),yv(i));
+    yv(i+1)= yv(i) + h/2*(f(xv(i),yv(i)) + f(xv(i+1),zv));
+  end
+  polinomioInterpolacion = dd_newton(xv, yv);
+  poli_n = matlabFunction(polinomioInterpolacion);
+  
+  hold on
+  ezplot(poli_n, intervalo);
+  stem(xv, yv, 'b');
+  
+endfunction 
+
+
+function polinomioInterpolacion = dd_newton(xv, yv)
+   pkg load symbolic;
+   syms x;
+    
+   n = length(xv);
+    
+   if length(xv) != length(yv)
+     display("Error, el tamano de los vectores xv y yv es diferente.");
+     return
+   endif
+  
+   # Se calcula el primer termino del polinomio de interpolacion.
+   polinomioInterpolacion = yv(1);
+
+   variable = 1;
+
+   # Cantidad de operaciones a realizar por cada iteracion.
+   contador = n - 1;
+   
+   for i=2:n
+     # Se van almacenando las variables (x-x0)(x-x1)...(x-xn-1).
+     variable = variable * (x-xv(i-1));
+     
+     # Lista para almacenar los multiplicadores.
+     multiplicadores = [];
+     
+     for j=1:contador
+      numerador = yv(j+1) - yv(j);
+      denominador = xv(j+i - 1) - xv(j);
+      multiplicadores = [multiplicadores numerador/denominador];
+     endfor
+     
+     # Se reduce en uno la cantidad de operaciones a realizar para 
+     # la siguiente iteracion.
+     contador = contador - 1;
+
+     # Se le agrega el nuevo termino al polinomio de interpolacion.
+     polinomioInterpolacion = polinomioInterpolacion + multiplicadores(1)*variable;
+
+     # Se actualiza la lista en y.
+     yv = multiplicadores;
+     
+   endfor
+
+   polinomioInterpolacion = expand(polinomioInterpolacion);
+   
 end
-
-
-%Grè´°fica de la aproximaciè´—n
-hold on
-plot(xv,yv,'r')
-
-%Soluciè´—n analè´øtica
-y_s=@(x) (x+1).^2-0.5*exp(x);
-%Graficar la solucion
-x_g=a:0.0001:b;
-y_g=y_s(x_g);
-plot(x_g,y_g,'b')
